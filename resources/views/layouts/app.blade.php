57432c8a4677b0b8295c63086912356ee8fa928b
ivan.horvat <ivan.horvat@eng.rs>
2018-08-16 15:39:33 +0200

    developement blog

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,6 +91,14 @@
                             <li class="list-group-item">
                                 <a href="{{ route('tags') }}">Tags</a>
                             </li>
+                            @if(Auth::user()->admin)
+                                <li class="list-group-item">
+                                    <a href="{{ route('users') }}">Users</a>
+                                </li>
+                                <li class="list-group-item">
+                                    <a href="{{ route('user.create') }}">New user</a>
+                                </li>
+                            @endif
                             <li class="list-group-item">
                                 <a href="{{ route('tag.create') }}">Create tag</a>
                             </li>
